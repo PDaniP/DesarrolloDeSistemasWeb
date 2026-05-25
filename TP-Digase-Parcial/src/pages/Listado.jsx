49e1ce9c18e3223a137d3fb0bd66ed8c313e426b
Pablo Picasso <pablop1184@gmail.com>
2026-05-25 18:35:18 -0300

    Trabajo en Listado.jsx

--- TP-Digase-Parcial/src/pages/Listado.jsx
+++ TP-Digase-Parcial/src/pages/Listado.jsx
@@ -8,3 +8,61 @@
 // - Pasar la lista al componente Lista.
 //
 // Debe mostrar solo información básica (ej: nombre).
+
+
+import { useEffect, useState } from "react";
+import { getItems } from "../services/api";
+import { Link } from "react-router-dom";
+
+const Listado = () => {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const res = await getItems();
+        setItems(res.data);
+      } catch (err) {
+        console.error(err);
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchItems();
+  }, []);
+
+  //Render condicional
+  if (loading) return <p>Cargando datos...</p>;
+  if (error) return <p>Error al conectar con la API</p>;
+
+  return (
+    <div>
+      <h1>Listado de Equipos</h1>
+
+      {items.length === 0 ? (
+        <p>No hay equipos cargados</p>
+      ) : (
+        <ul>
+          {items.map((item) => (
+            <li key={item.id}>
+              {/*SOLO UN ATRIBUTO (consigna)*/}
+              {item.nombre}
+
+              {" - "}
+
+              <Link to={`/detalle/${item.id}`}>
+                Ver más
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Listado;
