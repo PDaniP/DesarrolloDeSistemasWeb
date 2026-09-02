51f31cf2211bf2b29cecfaaf05e23b1bb1f4abd7
Pablo Picasso <pablop1184@gmail.com>
2026-09-01 21:56:26 -0300

    creacion middleware validar datos

--- SegundoCuatrimestre/src/middlewares/validacion.js
+++ SegundoCuatrimestre/src/middlewares/validacion.js
@@ -1,1 +1,66 @@
 //crear un middleware para validar el token de autenticacion
+
+// importo JWT y la voy a usar principalmente mediante jwt.verify()
+//que sirve para comporbar si un token es valido
+const jwt = require("jsonwebtoken");
+
+
+//creo el middleware con la estructura tipica de un middleware de Express
+//donde:
+//req ->informacion de la peticion
+//res ->respuesta que podemos enviar
+//next -> funcion que permite continuar con el siguiente middleware/controlador
+
+const validarToken = (req, res, next) => {
+
+    //obtenemos el token
+    const authHeader = req.headers.authorization;
+
+    //comprobamos si existe
+    if (!authHeader) {
+        return res.status(401).json({
+            mensaje: "Token no proporcionado"
+        });
+    }
+    //Separar Bearer del token
+    //con split(" ") dividimos el texto usando el espacio
+    //[
+    //"Bearer",
+    //"abc123"
+    //]
+    //con [1] obtenemos el segundo elemento del array que es el token
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({
+            mensaje: "Formato de token invalido"
+        });
+    }
+
+    try {
+        //verificamos el token
+        //JWT verifica:
+        //Que el token tenga un formato correcto.
+        //Que no haya sido modificado.
+        //Que haya sido firmado con nuestro JWT_SECRET.
+        //Que no esté expirado.
+        //Si todo está bien, devuelve el contenido que nosotros habíamos guardado dentro del JWT.
+        const usuario = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        req.usuario = usuario;
+
+        next();
+
+    } catch (error) {
+
+        return res.status(401).json({
+            mensaje: "Token invalido o expirado"
+        });
+
+    }
+}
+
+module.export = validarToken;
--- SegundoCuatrimestre/src/routes/usuarioRoutes.js
+++ SegundoCuatrimestre/src/routes/usuarioRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
 const busquedaPorEmail = require('../controllers/busquedaPorEmail');
+const validador = require('../middleware/validacion');
+
 
 //Definicion de la ruta
 router.post('/registro', usuarioController.registrar);
