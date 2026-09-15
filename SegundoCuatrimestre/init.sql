b936dcc3dfb175a867cb3a46f590cb534efabd3f
Pablo Picasso <pablop1184@gmail.com>
2026-09-14 22:13:57 -0300

    modificacion int.sql

--- SegundoCuatrimestre/init.sql
+++ SegundoCuatrimestre/init.sql
@@ -1,5 +1,15 @@
-CREATE DATABASE IF NOT EXISTS BBDD_DesSistWeb;
-USE BBDD_DesSistWeb;
+CREATE TABLE public.estado_usuario (
+    id integer GENERATED ALWAYS AS IDENTITY,
+    descripcion character varying(50) NOT NULL,
+    CONSTRAINT estado_usuario_id PRIMARY KEY (id),
+    CONSTRAINT descricion_unique UNIQUE (descripcion)
+);
+
+INSERT INTO public.estado_usuario (descripcion) VALUES
+('Activo'),
+('Inactivo'),
+('Suspendido');
+
 
 CREATE TABLE public.usuarios (
     id integer GENERATED ALWAYS AS IDENTITY,
@@ -10,27 +20,23 @@ CREATE TABLE public.usuarios (
     estado_id integer DEFAULT 1 NOT NULL,
     fecha_creacion timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT "Usuarios_pkey" PRIMARY KEY (id),
-    CONSTRAINT email_unique UNIQUE (email)
+    CONSTRAINT email_unique UNIQUE (email),
+    CONSTRAINT fk_usuario_estado
+        FOREIGN KEY (estado_id)
+        REFERENCES public.estado_usuario(id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
 );
 
-
-
-CREATE TABLE public.estado_usuario (
-    id integer GENERATED ALWAYS AS IDENTITY,
-    descripcion character varying(50) NOT NULL,
-    CONSTRAINT estado_usuario_id PRIMARY KEY (id),
-    CONSTRAINT descricion_unique UNIQUE (descripcion)
-);
-
-INSERT INTO public.estado_usuario (descripcion) VALUES
-('activo'),
-('inactivo'),
-('suspendido');
 
 CREATE TABLE public.publicaciones (
     id integer GENERATED ALWAYS AS IDENTITY,
     titulo character varying(150) NOT NULL,
     contenido text NOT NULL,
     autor_id integer NOT NULL,
-    CONSTRAINT publicaciones_pkey PRIMARY KEY (id)
+    CONSTRAINT publicaciones_pkey PRIMARY KEY (id),
+    CONSTRAINT publicaciones_autor_id_fkey
+        FOREIGN KEY (autor_id)
+        REFERENCES public.usuarios(id)
+        ON DELETE RESTRICT
 );
